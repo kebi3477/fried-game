1f65663bfcfdba4ce7a3522b7f37a9079c973af0
kebi3477 <kebi3477@naver.com>
2023-12-07 22:16:29 +0900

    feat: 원 생성 및 드래그 추가

--- public/js/run.js
+++ public/js/run.js
@@ -1,6 +1,8 @@
 import circlesConfig from '../config/circles.json.js';
-const { Engine, Render, World, Bodies, Runner, Events } = Matter;
+const { Engine, Render, World, Bodies, Runner, Events, MouseConstraint, Mouse, Body } = Matter;
 
+const START_CIRCLE_Y = 40;
+const GRAVITY = 2;
 const RESTITUTION = 0.2
 const MAX_LEFT = 0;
 const MAX_RIGHT = 400;
@@ -11,7 +13,7 @@ const WALL_WIDTH = 10;
 const engine = Engine.create({
     gravity: {
         x: 0,   
-        y: 2
+        y: GRAVITY
     }
 });
 const render = Render.create({
@@ -24,21 +26,56 @@ const render = Render.create({
         background: 'white' 
     }
 });
-
 const runner = Runner.create();
 
 const ground = Bodies.rectangle(MAX_WIDTH / 2, MAX_HEIGHT, MAX_WIDTH, WALL_WIDTH, { isStatic: true });
+const line = Bodies.rectangle(MAX_WIDTH / 2, 100, MAX_WIDTH, WALL_WIDTH, { isStatic: true, isSensor: true });
 const leftWall = Bodies.rectangle(MAX_LEFT, MAX_HEIGHT / 2, WALL_WIDTH, MAX_HEIGHT, { isStatic: true });
 const rightWall = Bodies.rectangle(MAX_RIGHT, MAX_HEIGHT / 2, WALL_WIDTH, MAX_HEIGHT, { isStatic: true });
 
 const circles = [];
+let draggableCircle = null;
+let isMouseDown = false;
 
-document.addEventListener('click', (event) => {
-    const x = event.clientX;
-    const circle = Bodies.circle(x, 20, circlesConfig[0].size, { restitution: RESTITUTION, render: { fillStyle: circlesConfig[0].color } });
+const mouse = Mouse.create(render.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+        stiffness: 0.2,
+        render: {
+            visible: false
+        }
+    } 
+})
+
+const createCircle = () => {
+    const circle = Bodies.circle(MAX_WIDTH / 2, START_CIRCLE_Y, circlesConfig[0].size, { isSleeping: true, restitution: RESTITUTION, render: { fillStyle: circlesConfig[0].color } });
+
+    draggableCircle = circle;
     circle.config = circlesConfig[0];
     circles.push(circle);
     World.add(engine.world, [circle]);
+}
+
+Events.on(mouseConstraint, 'mousedown', (event) => {
+    if (!isMouseDown) {
+        isMouseDown = true;
+    }
+});
+
+Events.on(mouseConstraint, 'mouseup', (event) => {
+    if (draggableCircle) {
+        draggableCircle.isSleeping = false;
+        draggableCircle = null;
+        isMouseDown = false;
+        setTimeout(createCircle, 1000);
+    }
+})
+
+Events.on(engine, 'beforeUpdate', (event) => {
+    if (draggableCircle && isMouseDown) {
+        Body.setPosition(draggableCircle, { x: mouse.position.x, y: START_CIRCLE_Y });
+    }
 });
 
 Events.on(engine, 'collisionStart', (event) => {
@@ -66,7 +103,9 @@ Events.on(engine, 'collisionStart', (event) => {
     }
 });
 
-World.add(engine.world, [ground, leftWall, rightWall ]);
+World.add(engine.world, [ground, line, leftWall, rightWall, mouseConstraint]);
 
 Render.run(render);
 Runner.run(runner, engine);
+
+createCircle();
